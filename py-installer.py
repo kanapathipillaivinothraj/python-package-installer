8f7d94821f7027d7885a8966fa6c91a9ef0607c4
Vinothraj Kanapathipillai <kanapathipillaivinothraj@gmail.com>
2023-05-06 17:26:01 +0530

    feat: create a function for need packages writer

--- py-installer.py
+++ py-installer.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+import subprocess
+import sys
 
 
 ''' BASE_DIR is get the local file path'''
@@ -7,6 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # print(BASE_DIR)
 
 ''' file_path is jois the working env path '''
-file_path = os.path.join(BASE_DIR/'python-package-installer/installed_packages.txt')
+basic_packages_path = os.path.join(BASE_DIR/'python-package-installer/installed_packages.txt')
 # print(file_path)
 
+''' Write the packaes name in `need_packages.txt` you want to download '''
+input = str(input("Enter the Package name ? "))
+file_path_need = os.path.join(BASE_DIR/'python-package-installer/need_packages.txt')
+with open(file_path_need,"w") as file_writing:
+    # print(f"open => [{file_path_need}] ")
+    input_conert = input.lower()
+    file_writing.write(f"{input_conert}\n")
